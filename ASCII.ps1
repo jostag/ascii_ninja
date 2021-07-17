875a4a1fc420f089f062e936872dc18d5190d096
jostag <35642154+jostag@users.noreply.github.com>
2021-07-17 16:23:30 -0400

    Add files via upload

---
+++ ASCII.ps1
@@ -0,0 +1,34 @@
+﻿Write-Host "                                           
+
+
+                                            MMMMMMMM                 MMM
+                                          MMMMMMMMMMMMMM            MMM
+                                         MMMMMMMMMMMMMMMMM         MMM
+                                        MMMMMMMMMMMMMMMMMMM       MMM
+                                       MMMMMMMMMMMMMMMMMMMMM     MMM
+                                      MMMMMMMMMMMMMMMMMMMMMMMM   MM
+                                     MMMMMMMMMMMMMMMMMMMMMMMMMM M
+                                     MMMMMMMMMMMMMMMMMMMMMMMMM
+                                    MMMMMMMMMMMMMMMMMMMMMMMMM
+                                                 M    MMMMMM   M
+                                       &&     &&&      MMMM   MM
+                                                       MMM   MMM
+                                                     MMMMM   MMM
+                                    MMMM         MMMMMMMM   MMM
+                                     MMMMMMMMMMMMMMMMMMM   MMM
+                                       MMMMMMMMMMMMMMMM   MMM
+                                        MMMMMMMMMMMMMM  MMMM
+                                          MMMMMMMMMMM  MMMM
+                                            MMMMM       MM
+                                           MMMMMM    \   MMM
+                                          MMMMMMMM  \   MMMMM
+                                        MMMMMMMMMMM    MMMMMM
+                                       MMMMMMMMM      MMMMMMMMM
+                                          MMMMM    \  MMMMMMM
+                                            MMM  \  MMMMMMMMMMM
+                                           MMMMMM  MMMMMMMMMMMMMM
+                            MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                            MMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMMM
+                            
+                            
+                            " -ForegroundColor DarkMagenta
